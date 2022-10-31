a3210656aafc406e7c6031a323ce7431d64b9eae
Gabriel Løsnesløkken <gabriell@uia.no>
2022-10-31 09:58:13 +0100

    Create AdministrateController

---
+++ prosjekt/Controllers/AdministrateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prosjekt.Controllers;
+
+public class AdministrateController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
