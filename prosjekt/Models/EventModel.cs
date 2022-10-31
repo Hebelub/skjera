90b3288cefc55484e2b5d778b2304dae15d41c02
Gabriel Løsnesløkken <gabriell@uia.no>
2022-10-31 09:58:56 +0100

    Create EventModel.cs

---
+++ prosjekt/Models/EventModel.cs
@@ -0,0 +1,27 @@
+﻿namespace prosjekt.Models;
+
+public class EventModel
+{
+    public EventModel()
+    {
+    }
+
+
+    /// <summary>
+    /// The DateTime when this event was posted
+    /// </summary>
+    private DateTime TimeCreated { get; set; } = DateTime.Now;
+
+    /// <summary>
+    /// This is null if the event has never been altered
+    /// It is DateTime of the time it was last edited 
+    /// </summary>
+    private DateTime? LastTimeEdited { get; set; }
+
+    private bool IsEdited { get => LastTimeEdited != null; }
+
+    /// <summary>
+    /// The organization who organizes this Event
+    /// </summary>
+    private OrganizationModel organizer { get; set; }
+}
