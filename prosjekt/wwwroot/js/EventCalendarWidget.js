ce89cc0e88faa6689ce5da373ffd501c84bea2b9
Aziz Azizi <azizullaha@uia.no>
2022-11-08 09:41:51 +0100

    update calender

--- prosjekt/wwwroot/js/EventCalendarWidget.js
+++ prosjekt/wwwroot/js/EventCalendarWidget.js
@@ -55,7 +55,7 @@
         var self = this;
 
         this.events.forEach(function(ev) {
-            ev.date = self.current.clone().date(Math.random() * (29 - 1) + 1);
+            ev.date = self.current.clone().date(ev.date);
         });
 
 
@@ -321,25 +321,12 @@
 
 !function() {
     var data = [
-        { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
-        { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
-        { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
-        { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
-
-        { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
-        { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
-        { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
-        { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
-
-        { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
-        { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
-        { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
-        { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
-
-        { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
-        { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
-        { eventName: 'Teach Kids to Code', calendar: 'Other', color: 'green' },
-        { eventName: 'Startup Weekend', calendar: 'Other', color: 'green' }
+        { eventName: 'Game vs Portalnd', date: '09', time: '11:00', color: 'blue' },
+        { eventName: 'Game vs Houston', date: '09', time: '12:00', color: 'green' },
+        { eventName: 'Game vs Denver', date: '09', time: '13:00', color: 'orange' },
+        { eventName: 'Game vs San Antonio', date: '09', time: '14:00', color: 'yellow' },
+        
+        
     ];
 
 
