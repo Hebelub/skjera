ce2b025d6fca7fd0061a1827d7b8cab4f5ba5764
Gabriel Løsnesløkken <gabriell@uia.no>
2022-11-27 21:10:13 +0100

    Remove some comments

--- prosjekt/Controllers/OrganizationController.cs
+++ prosjekt/Controllers/OrganizationController.cs
@@ -45,10 +45,8 @@ namespace prosjekt.Controllers
         {
             return "From [HttpPost]Index: filter on " + searchString;
         }
-        
-        
-        
-        
+
+
         // GET: Organization/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -77,8 +75,6 @@ namespace prosjekt.Controllers
         }
 
         // POST: Organization/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
@@ -128,8 +124,6 @@ namespace prosjekt.Controllers
         }
 
         // POST: Organization/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
