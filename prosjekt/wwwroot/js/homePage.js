12c8422d35191dea357939b454f1f708d6aef53b
Gabriel Løsnesløkken <gabriell@uia.no>
2022-12-02 14:23:21 +0100

    Working js

--- prosjekt/wwwroot/js/homePage.js
+++ prosjekt/wwwroot/js/homePage.js
@@ -1,23 +1,74 @@
-﻿function getHomePageCheckboxSelection() {
-    
-    console.log(document.getElementById("homePageCheckboxAttending"));
-    
-    let attendingChecked = document.getElementById("homePageCheckboxAttending").checked;
-    let organizationsChecked = document.getElementById("homePageCheckboxOrganizationsYouFollow").checked;
-    let otherChecked = document.getElementById("homePageCheckboxOther").checked;
-    
-    console.log("isChecked", attendingChecked, organizationsChecked, otherChecked);
+﻿
+let _calendar = new Calendar('#calendar');
+let _fetchedEvents = [];
+
+function getHomePageCheckboxSelection() {
+    return {
+        attending: document.getElementById("homePageCheckboxAttending").checked, 
+        organizations: document.getElementById("homePageCheckboxOrganizationsYouFollow").checked, 
+        pastEvents: document.getElementById("homePageCheckboxPast").checked,
+        other: document.getElementById("homePageCheckboxOther").checked
+    };
 }
 
-function getFirstAndLastDateOfCalendar() {
+function eventToJsEvent(events) {
+    events.forEach((event) => {
+        event.startTime = moment(event.startTime, 'YYYY-MM-DDTHH:mm:ss');
+        event.endTime = moment(event.endTime, 'YYYY-MM-DDTHH:mm:ss');
+        
+        if (event.endTime.isAfter(moment())) {
+            if (event.startTime.isBefore(moment())) {
+                event.color = 'red';
+                event.calendar = 'Active Event';
+            }
+            else if (event.isUserAttending) {
+                event.color = 'blue';
+                event.calendar = 'Attending';
+            }
+            else if (event.isUserFollowingOrganizer) {
+                event.color = 'green';
+                event.calendar = 'Following';
+            }
+            else {
+                event.color = 'yellow';
+                event.calendar = 'Other Events';
+            }
+        }
+        else {
+            event.color = 'gray';
+            event.calendar = 'Ended';
+        }
+    });
     
+    return events;
 }
 
-function fetchHomePageData() {
-    getFirstAndLastDateOfCalendar();
+async function fetchEvents() {
+    let events = await getEventsBetweenDates(
+        _calendar.getFirstDateOfCalendar(), 
+        _calendar.getLastDateOfCalendar()
+    );
+
+    eventToJsEvent(events);
+    
+    return events;
 }
 
-function drawHomePage() {
-    var siteData = fetchSiteData();
+function includeOnlyCheckedEvents(events) {
     getHomePageCheckboxSelection();
+
+    // Heisann
+    
 }
+
+async function updateHomePage() {
+    _fetchedEvents = await fetchEvents();
+
+    includeOnlyCheckedEvents(_fetchedEvents);
+    
+    _calendar.events = _fetchedEvents;
+    
+    _calendar.draw();
+}
+
+updateHomePage();
