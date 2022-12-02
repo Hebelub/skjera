5dd7e758444332897a8d4a5ce2332246e18902f4
Gabriel Løsnesløkken <gabriell@uia.no>
2022-12-02 13:24:56 +0100

    Romove _calendar partial view and make js homePage functions able to be called

---
+++ prosjekt/wwwroot/js/homePage.js
@@ -0,0 +1,23 @@
+﻿function getHomePageCheckboxSelection() {
+    
+    console.log(document.getElementById("homePageCheckboxAttending"));
+    
+    let attendingChecked = document.getElementById("homePageCheckboxAttending").checked;
+    let organizationsChecked = document.getElementById("homePageCheckboxOrganizationsYouFollow").checked;
+    let otherChecked = document.getElementById("homePageCheckboxOther").checked;
+    
+    console.log("isChecked", attendingChecked, organizationsChecked, otherChecked);
+}
+
+function getFirstAndLastDateOfCalendar() {
+    
+}
+
+function fetchHomePageData() {
+    getFirstAndLastDateOfCalendar();
+}
+
+function drawHomePage() {
+    var siteData = fetchSiteData();
+    getHomePageCheckboxSelection();
+}
